80537a4b2531ba25882dd909cfad25b5c263c3b8
helixedra <greencastle08@gmail.com>
2020-02-11 23:11:42 +0200

    upd

--- routes/products.js
+++ routes/products.js
@@ -13,11 +13,11 @@ function toArray(str) {
 }
 
 function convertData(data) {
-    if(data !== null) {
-        if(data.length > 1 ) {
-            return {...data}
+    if (data !== null) {
+        if (data.length > 1) {
+            return { ...data }
         } else {
-            return {data}
+            return { data }
         }
     } else {
         return false
@@ -34,7 +34,7 @@ router.get('/c/:ref', async function (req, res) {
     let getAllCategories = await getData('SELECT * FROM categories', false)
 
     let categoryData = getAllCategories.filter(category => category.ref === req.params.ref)
-    categoryData = {...categoryData[0]}
+    categoryData = { ...categoryData[0] }
 
     if (categoryData !== undefined) {
 
@@ -43,11 +43,11 @@ router.get('/c/:ref', async function (req, res) {
             req.query.sort = 'rating'
 
             let productsData = await getData('SELECT * FROM products WHERE category = ? AND primary_item = 1 ORDER BY ?? ASC', [categoryData.id, req.query.sort])
-            
-            if(productsData === null) {
+
+            if (productsData === null) {
                 productsData = false
             } else {
-                productsData = {productsData}
+                productsData = { productsData }
             }
 
             res.render('category', {
@@ -62,11 +62,11 @@ router.get('/c/:ref', async function (req, res) {
         } else if (req.query.sort === 'priceup') {
 
             let productsData = await getData('SELECT * FROM products WHERE category = ? AND primary_item = 1 ORDER BY price ASC', categoryData.id)
-            
-            if(productsData === null) {
+
+            if (productsData === null) {
                 productsData = false
             } else {
-                productsData = {productsData}
+                productsData = { productsData }
             }
 
             res.render('category', {
@@ -81,11 +81,11 @@ router.get('/c/:ref', async function (req, res) {
         } else if (req.query.sort === 'pricedown') {
 
             let productsData = await getData('SELECT * FROM products WHERE category = ? AND primary_item = 1  ORDER BY price DESC', categoryData.id)
-            
-            if(productsData === null) {
+
+            if (productsData === null) {
                 productsData = false
             } else {
-                productsData = {productsData}
+                productsData = { productsData }
             }
 
             res.render('category', {
@@ -118,24 +118,31 @@ router.get('/p/:id', async function (req, res) {
 
         // Get all catagories data from DB
         let getAllCategories = await getData('SELECT * FROM categories', false)
-        
+
         // console.log(getAllCategories); //array of obj
 
         // Select data for specific category
         let categoryData = getAllCategories.filter(category => category.id === productData.category)
-        categoryData = {...categoryData[0]}
+        categoryData = { ...categoryData[0] }
+
+        // console.log(productData);
+
 
         // Get data from DB of all possible color available for selected product
         let productColorOptions = await getData('SELECT products.id, products.color, products.product_group, products.ref, products.size, colors.color_id, colors.color_name, colors.color_hex FROM products JOIN colors ON products.color = colors.color_id WHERE products.product_group = ?', productData.product_group)
 
+
+        // console.log(productColorOptions);
+
         // Get array of colors id available for this product in group
-        let availableColorsAll = productColorOptions.map(item => item.color)
+        let availableColorsAll = (Array.isArray(productColorOptions)) ? productColorOptions.map(item => item.color) : productColorOptions.color
+
 
         // Filter colors id (removing duplicates)
-        let availableColors = availableColorsAll.filter((item, index) => availableColorsAll.indexOf(item) === index)
-          
+        let availableColors = (Array.isArray(availableColorsAll)) ? availableColorsAll.filter((item, index) => availableColorsAll.indexOf(item) === index) : availableColorsAll
+
         // Create new array with data to provide as product color options in template
-        let colorOptions = availableColors.map(id => productColorOptions.find(item => item.color === id))
+        let colorOptions = (Array.isArray(availableColorsAll)) ? availableColors.map(id => productColorOptions.find(item => item.color === id)) : productColorOptions
 
         // Get data from DB for all available size options for selected product
         let sizeOptions = await getData('SELECT products.id, products.ref, products.color, products.size, products.product_group, sizes.size_id, sizes.size_name FROM products JOIN sizes ON products.size = sizes.size_id WHERE products.color = ? AND products.product_group = ?', [req.query.color, productData.product_group])
@@ -150,15 +157,15 @@ router.get('/p/:id', async function (req, res) {
                 product: productData,
                 categories: getAllCategories,
                 category: categoryData,
-                colors: {...colorOptions},
+                colors: (Array.isArray(colorOptions)) ? [...colorOptions] : [colorOptions],
                 sizes: convertData(sizeOptions),
                 thumbs: toArray(productData.images),
-                relatedProducts: {relatedProducts}
+                relatedProducts: { relatedProducts }
             })
         } else {
             res.status(404).render('404')
         }
-    // Render 404 if query string parameters missing
+        // Render 404 if query string parameters missing
     } else {
         res.status(404).render('404')
     }
